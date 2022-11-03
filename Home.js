2c6423c2ed22a5a9a4cf549cadfd8c2078592476
MahipalTarar <117256955+MahipalTarar@users.noreply.github.com>
2022-11-04 01:36:37 +0530

    home.js updated by mahipal

--- Home.js
+++ Home.js
@@ -7,11 +7,21 @@ function Home() {
             <div className='home_container'>
                 <img className= 'home_image'
                  src='https://imgs.search.brave.com/RtFETTrYGMHSUJ67T83MIQzePv91qvDp0uee5dHnpcQ/rs:fit:1200:900:1/g:ce/aHR0cHM6Ly90aGVk/aWdpdGFseS5zMy51/cy1lYXN0LTIuYW1h/em9uYXdzLmNvbS9p/bWcvMjAyMS8wMS8y/MTAwNDkwOS9wcmlt/ZS12aWRlby1tb2Jp/bGUtZWRpdGlvbi5q/cGc' alt="prime" />
+                 
+                 <div className="home_row">
+                    {/* Product */}
+                    {/* Product */}
+                    {/* Product */}
+                 </div>
+                 <div className="home_row">
+                    {/* Product */}
+
+                 </div>
             </div>
 
 
         </div>
-    )
+    );
 }
 
-export default Home
+export default Home;
