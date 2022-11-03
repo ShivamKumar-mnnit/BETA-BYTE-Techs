3d31d8828e732d2ec6bef35ecf6d0f86f6da3981
MICKEY-mi <106375725+MICKEY-mi@users.noreply.github.com>
2022-11-04 00:12:50 +0530

    construction of home page

---
+++ Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Home.css"
+
+function Home() {
+    return (
+        <div className='home'>
+            <div className='home_container'>
+                <img className= 'home_image'
+                 src='https://imgs.search.brave.com/RtFETTrYGMHSUJ67T83MIQzePv91qvDp0uee5dHnpcQ/rs:fit:1200:900:1/g:ce/aHR0cHM6Ly90aGVk/aWdpdGFseS5zMy51/cy1lYXN0LTIuYW1h/em9uYXdzLmNvbS9p/bWcvMjAyMS8wMS8y/MTAwNDkwOS9wcmlt/ZS12aWRlby1tb2Jp/bGUtZWRpdGlvbi5q/cGc' alt="prime" />
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Home
